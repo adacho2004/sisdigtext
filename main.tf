22a4a563c21abea2b309f39c4fbcd36e95c3ff90
adacho2004 <61000022+adacho2004@users.noreply.github.com>
2020-02-13 15:52:11 +0900

    Add files via upload

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "ibm_resource_instance" "resource_instance" {
   name              = "${var.service_instance_name}"
   service           = "sysdig-monitor"
   plan              = "graduated-tier"
-  location          = "jp_tok"
+  location          = "jp-tok"
   resource_group_id = "${data.ibm_resource_group.group.id}"
   tags              = ["${var.tag1}", "${var.tag2}"]
 
