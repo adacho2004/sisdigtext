bc44ea0f17799809cca4e6462b7a676f61ac99c1
adacho2004 <61000022+adacho2004@users.noreply.github.com>
2020-02-13 15:04:14 +0900

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "ibm" {
+  generation = 1
+}
+data "ibm_resource_group" "group" {
+  name = "infra_prottype"
+}
+
+resource "ibm_resource_instance" "resource_instance" {
+  name              = var.service_instance_name
+  service           = "sysdig_monitor"
+  plan              = "graduated-tier"
+  location          = "jp_tok"
+  resource_group_id = data.ibm_resource_group.group.id
+  tags              = [var.tag1, var.tag2]
+
+  //User can increase timeouts 
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "service_instance_name" {
+  default = "tokyo_railway"
+}
+
+variable "tag1" {
+  default = "tokyo_railway"
+}
+
+variable "tag2" {
+default = "tokyo"
+}
