543a99da452ded2fc42376b09bbc05bf4b205788
adacho2004 <61000022+adacho2004@users.noreply.github.com>
2020-02-13 15:16:40 +0900

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
 variable "service_instance_name" {
+  type = "string"
+  description = "インスタンス名"
   default = "tokyo_railway"
 }
 
 variable "tag1" {
+  type = "string"
+  description = "インスタンスにつけるタグ名"
   default = "tokyo_railway"
 }
 
 variable "tag2" {
-default = "tokyo"
+  type = "string"
+  description = "インスタンスにつけるタグ名"
+  default = "tokyo"
 }
